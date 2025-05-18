b583376cf204bbd17027eab13a9a8ef0f97af427
K-Gull <kirangull568@gmail.com>
2025-05-18 18:07:01 +0500

    change

--- resources/views/Category/main.blade.php
+++ resources/views/Category/main.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <meta name="description" content=""/>
   <meta name="author" content=""/>
-  <title>Admin @yield('title')</title>
+  <title>Admin | @yield('title')</title>
   <!--favicon-->
   <link rel="icon" href="Admin/images/favicon.ico" type="image/x-icon">
   <!-- Vector CSS -->
