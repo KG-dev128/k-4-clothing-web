312ef3c97105826c2355883d9725af5dc2fbc40a
K-Gull <kirangull568@gmail.com>
2025-05-18 17:28:49 +0500

    create.blade.php

--- resources/views/Category/create.blade.php
+++ resources/views/Category/create.blade.php
@@ -16,22 +16,6 @@
             <label for="input-6">Name</label>
             <input type="text" class="form-control form-control-rounded" id="input-6" placeholder="Enter Your Name">
            </div>
-           <div class="form-group">
-            <label for="input-7">Email</label>
-            <input type="text" class="form-control form-control-rounded" id="input-7" placeholder="Enter Your Email Address">
-           </div>
-           <div class="form-group">
-            <label for="input-8">Mobile</label>
-            <input type="text" class="form-control form-control-rounded" id="input-8" placeholder="Enter Your Mobile Number">
-           </div>
-           <div class="form-group">
-            <label for="input-9">Password</label>
-            <input type="text" class="form-control form-control-rounded" id="input-9" placeholder="Enter Password">
-           </div>
-           <div class="form-group">
-            <label for="input-10">Confirm Password</label>
-            <input type="text" class="form-control form-control-rounded" id="input-10" placeholder="Confirm Password">
-           </div>
            <div class="form-group py-2">
              <div class="icheck-material-white">
             <input type="checkbox" id="user-checkbox2" checked=""/>
