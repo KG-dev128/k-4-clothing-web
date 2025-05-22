9c9f9fd2191d82e1d485b82349de448b1151f189
K-Gull <kirangull568@gmail.com>
2025-05-22 16:53:15 +0500

    changes

---
+++ app/Http/Controllers/Product1Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product_1;
+use Illuminate\Http\Request;
+
+class Product1Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product_1  $product_1
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product_1 $product_1)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product_1  $product_1
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product_1 $product_1)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product_1  $product_1
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product_1 $product_1)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product_1  $product_1
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product_1 $product_1)
+    {
+        //
+    }
+}
